316b8b32552691de1c2bf3fa48d0b035ce98e41f
Tony Le <tle@kabbage.com>
2020-09-30 21:48:02 -0400

    3, 4

---
+++ src/1/3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void urlify(char *string, int trueLength)
+{
+    int end = std::strlen(string) - 1;
+
+    for (int i = trueLength - 1; i >= 0; i--)
+    {
+        if (string[i] != ' ')
+        {
+            string[end] = string[i];
+            end--;
+        }
+        else
+        {
+            string[end] = '0';
+            string[end - 1] = '2';
+            string[end - 2] = '%';
+            end -= 3;
+        }
+    }
+}
+
+int main()
+{
+    char test[] = "Mr John Smith    ";
+    urlify(test, 13);
+    std::cout << test;
+}
---
+++ src/1/4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+bool isPermutationOfPalindrome(char *string)
+{
+    std::vector<bool> charDict(128, false);
+
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+        char c = tolower(string[i]);
+        if (c != ' ')
+        {
+            charDict[(int)c] = !charDict[(int)c];
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < 128; i++)
+    {
+        count += charDict[i];
+    }
+
+    return count < 2;
+}
+
+int main()
+{
+    std::cout << isPermutationOfPalindrome("Tact Coa");
+}
